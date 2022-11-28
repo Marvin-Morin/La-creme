5c74b921e47404e688268f5eb5400c339c1f135a
Marvin Morin <94224087+Marvin-Morin@users.noreply.github.com>
2022-11-28 18:52:49 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ FlecheMenuNav.addEventListener('click', () => {
     Liens.style.display = (check = check) ? "flex" : "none";
     Liens.style.animationName = (check = check) ?  'menuburgeranimation' : 'menuburgeranimationclose';
     Liens.style.animationDuration = (check = check) ?  '0.6s' : '0.4s';
-    FlecheMenuNav.src = (check = check) ? 'image/croix.png' : 'image/burger.png';
+    FlecheMenuNav.src = (check = check) ? 'image/croix.png' : 'image/icons8-menu-30.png';
 
     check=!check;
 
